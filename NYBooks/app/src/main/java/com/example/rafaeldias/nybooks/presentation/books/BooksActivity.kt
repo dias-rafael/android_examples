5c78b550d10274052aea2be288acf2a451a65ac8
Rafael Dias <rafael.dias@outlook.com>
2022-05-07 10:36:06 -0300

    Layout: Toolbar e RecyclerView

--- NYBooks/app/src/main/java/com/example/rafaeldias/nybooks/presentation/books/BooksActivity.kt
+++ NYBooks/app/src/main/java/com/example/rafaeldias/nybooks/presentation/books/BooksActivity.kt
@@ -3,10 +3,19 @@ package com.example.rafaeldias.nybooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rafaeldias.nybooks.R
+import com.example.rafaeldias.nybooks.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_books)
+        //setContentView(R.layout.activity_books)
+
+        val binding = ActivityBooksBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.toolbarMain.title = "Books"
+        setSupportActionBar(binding.toolbarMain)
     }
 }
